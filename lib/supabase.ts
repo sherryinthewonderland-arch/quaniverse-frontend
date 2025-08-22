1d25d29979ed9bc4804632d93c4227eaeeb56cd6
sherryinthewonderland-arch <sherryinthewonderland@gmail.com>
2025-08-23 06:29:15 +0800

    Add files via upload

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,8 @@
-import { createClient } from '@supabase/supabase-js';
+// lib/supabase.ts
+import { createBrowserClient } from '@supabase/ssr'
+import { Database } from '@/types_db'
 
-export const supabase = createClient(
+export const supabase = createBrowserClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+)
